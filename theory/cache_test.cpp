81ecf8cc6cde2d5b6a519f84fed22de507203b8a
viogp <violetagp@protonmail.com>
2025-03-11 17:16:15 +0100

    Improved cache_test.cpp to see the cache misses more clearly

--- theory/cache_test.cpp
+++ theory/cache_test.cpp
@@ -1,9 +1,10 @@
-// Compilation: g++ -o p cache_test.cpp
+// Compilation diabling optimisations: g++ -O0 -o p cache_test.cpp
 #include <iostream>
 #include <cmath>
 #include <chrono>
 #include <iomanip>
 #include <random>  // Library to produce random numbers
+#include <vector>  // For avoiding stack overflow issues with large arrays
 
 using namespace std;
 using namespace chrono;
@@ -13,22 +14,27 @@ int main(){
   double time=0.;
   high_resolution_clock::time_point begin, end;
   
-  int nrow=2, ncol=3;
-  int DD[nrow][ncol] = {{0}};
+  int nrow=10000, ncol=10000;
+  vector<vector<int>> DD(nrow, vector<int>(ncol, 0));
 
   cout << scientific << setprecision(2);
 
-  // Create a random number generator engine
+  // Pre-generate random numbers followint a uniform dis. between 0 and 99
   random_device rd;  // Seed generator
   mt19937 gen(rd()); // Mersenne Twister engine
-  // Define a distribution (e.g., uniform distribution between 0 and 99)
   uniform_int_distribution<int> distribution(0, 99);
-
+  vector<vector<int>> randomNumbers(nrow, vector<int>(ncol, 0));
+  for (i = 0; i < nrow; i++) {
+    for (j = 0; j < ncol; j++) {
+      randomNumbers[i][j] = randomNumbers[i][j];
+    }
+  }
+    
   // Populate array in reverse order and bad data alligment, with random numbers
   begin = high_resolution_clock::now(); // Start the timer
   for (j=ncol-1; j>=0; j--){
     for(i=nrow-1; i>=0; i--){
-      DD[i][j]= distribution(gen);
+      DD[i][j]= randomNumbers[i][j];
     }
   }
   end = high_resolution_clock::now();   // End the timer
@@ -39,7 +45,7 @@ int main(){
   begin = high_resolution_clock::now(); // Start the timer
   for (i=nrow-1; i>=0; i--){
     for(j=ncol-1; j>=0; j--){
-      DD[i][j]= distribution(gen);
+      DD[i][j]= randomNumbers[i][j];
     }
   }
   end = high_resolution_clock::now();   // End the timer
@@ -50,7 +56,7 @@ int main(){
   begin = high_resolution_clock::now(); // Start the timer
   for (j=0; j<ncol; j++){
     for(i=0 ; i<nrow ;i++){
-      DD[i][j]= distribution(gen);
+      DD[i][j]= randomNumbers[i][j];
     }
   }
   end = high_resolution_clock::now();   // End the timer
@@ -61,7 +67,7 @@ int main(){
   begin = high_resolution_clock::now(); // Start the timer
   for (i=0; i<nrow; i++){
     for(j=0 ; j<ncol; j++){
-      DD[i][j]= distribution(gen);
+      DD[i][j]= randomNumbers[i][j];
     }
   }
   end = high_resolution_clock::now();   // End the timer
